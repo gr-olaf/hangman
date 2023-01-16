717852701317f4d09f7cb212530a6a41c1f61a3a
Alexandr Zaycev <hunter290592@yandex.ru>
2023-01-16 19:13:56 +0300

    first commit

---
+++ src/HangmanWord.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+type HangmanWordProps = {
+	guessedLetters: string[];
+	wordToGuess: string;
+	reveal: boolean;
+};
+
+const HangmanWord = ({
+	guessedLetters,
+	wordToGuess,
+	reveal = false,
+}: HangmanWordProps) => {
+	return (
+		<div
+			style={{
+				display: 'flex',
+				gap: '0.25rem',
+				fontSize: '6rem',
+				fontWeight: 'bold',
+				textTransform: 'uppercase',
+				fontFamily: 'monospace',
+			}}
+		>
+			{wordToGuess.split('').map((letter, index) => (
+				<span
+					style={{
+						borderBottom: '0.1em solid black',
+						marginInline: '0.1em',
+					}}
+					key={index}
+				>
+					<span
+						style={{
+							visibility:
+								guessedLetters.includes(letter) || reveal
+									? 'visible'
+									: 'hidden',
+							color:
+								!guessedLetters.includes(letter) && reveal ? 'red' : 'black',
+						}}
+					>
+						{letter}
+					</span>
+				</span>
+			))}
+		</div>
+	);
+};
+
+export default HangmanWord;
